e15f597f73514d5f307e58da8adf27ad869100e2
Samiur Rashid <samrashid@gmail.com>
2017-10-18 19:43:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var ranks = document.getElementById('app').querySelector('ul.ranked-list li');
   for (let i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = (i+n).toString();
+    console.log(ranks[i].innerHTML);
   }
 }
